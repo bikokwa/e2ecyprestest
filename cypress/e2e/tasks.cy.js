9b2022594b765c4005ba00bcdfe3f7856509a83d
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-09-15 19:23:40 +0300

    proper asertions

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -4,5 +4,8 @@ describe("tasks management", () => {
   it("should open and close the new task modal", () => {
     cy.visit("http://localhost:5173/");
     cy.contains("Add Task").click({ force: true });
+    cy.get(".backdrop").click({ force: true });
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
   });
 });
